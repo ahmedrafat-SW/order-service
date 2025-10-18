c035a234245328df97e1dfdf28fb0debbac8aeee
Ahmed Rafat <ahmed.rafat@asset.com.eg>
2025-10-18 17:00:29 +0300

    added application entrypoint

---
+++ src/main/java/com/dev/orderservice/OrderServiceApplication.java
@@ -0,0 +1,17 @@
+package com.dev.orderservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+@SpringBootApplication
+@EnableTransactionManagement
+@EnableAsync
+public class OrderServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrderServiceApplication.class, args);
+	}
+
+}
